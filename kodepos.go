66516f60116edf96a7579f757712a0b21bffd613
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-11-25 14:16:34 +0700

    Inisialitasi variabel kecamatan

--- kodepos.go
+++ kodepos.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
+type kecamatan struct {
+	idKecamatan     string
+	kecamatanDaerah string
+}
+
 type kodepos struct {
-	idKodePos int
-	kecamatan string
-	kelurahan string
-	kodepos   string
+	idKodePos   int
+	idKecamatan string
+	kelurahan   string
+	kodepos     string
 }
 
 func main() {
